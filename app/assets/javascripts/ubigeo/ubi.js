bc900e455b2002329ea33c6485dc786c16de70ad
fernando <luisblz93@gmail.com>
2014-09-24 16:10:44 -0700

    agregada ubigeo con scripts

---
+++ app/assets/javascripts/ubigeo/ubi.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	$('#geocomplete').geocomplete({
+	map: ".map_canvas"
+	});
+
+	/*$("#find").click(function(){
+		$("#geocomplete").trigger("geocode");
+	});*/
+});
